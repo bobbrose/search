29ab6b6e084de8972264703c5b1c186556faa27d
Bob Rose <bobbrose@gmail.com>
2023-06-11 09:51:36 -0600

    About dialog

--- src/AboutBox.js
+++ src/AboutBox.js
@@ -1,6 +1,14 @@
 import React, { useState } from 'react';
 import Modal from 'react-modal';
 
+const customStyles = {
+    content: {
+      width: '500px', 
+      height: '200px',
+      margin: 'auto',
+    },
+  };
+  
 // Show a react modal about box with some info about the author and purpose.
 const AboutBox = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -16,9 +24,9 @@ const AboutBox = () => {
     return (
         <div>
             <button onClick={handleOpenModal}>About</button>
-            <Modal isOpen={isOpen} onRequestClose={handleCloseModal}>
+            <Modal isOpen={isOpen} onRequestClose={handleCloseModal} style={customStyles} >
                 <h2>Searchy</h2>
-                <p>A simple search interface prepared for <a href="https://you.com">You.com</a> by <a href="https://bobbrose.com">Bob Rose</a>.</p>
+                <p>A simple search interface for wikipedida. <a href="https://bobbrose.com">Bob Rose</a></p>
                 <button onClick={handleCloseModal}>Close</button>
             </Modal>
         </div>
