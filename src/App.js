b44d974fe58c6266a71aa8176c4d1eeeef825111
Bob Rose <bobbrose@gmail.com>
2023-06-29 00:20:43 -0600

    History working

--- src/App.js
+++ src/App.js
@@ -1,36 +1,46 @@
 import './Searchy.css';
 import AboutBox from './AboutBox';
+import History from './History';
 import WikimediaSearch from './WikimediaSearch';
+import React, { useState } from 'react';
+import { useRef } from 'react';
 
 // Search app using React, to search wikipedia and show the result.
 function App() {
+  const [searchHistory, setSearchHistory] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const inputRef = useRef(null);
+
   return (
     <div>
       <table className='searchTable'>
-        <tr className='search'>
-          <td className='left'></td>
-          <td classname='middle'>
-            <h1 className='title'>Searchy</h1>
-          </td>
-          <td>
-            <div className='about'>
-              <AboutBox />
-            </div>
-          </td>
-        </tr>
-        <tr>
-          <td className='history'>
-            <h3 >History</h3>
-          </td>
-          <td >
-            <div>
-              <WikimediaSearch />
-            </div>
-          </td>
-        </tr>
+        <tbody>
+          <tr className='search'>
+            <td className='left'></td>
+            <td className='middle'>
+              <h1 className='title'>Searchy</h1>
+            </td>
+            <td>
+              <div className='about'>
+                <AboutBox />
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td className='left'>
+              <History searchHistory={searchHistory} inputRef={inputRef} setSearchTerm={setSearchTerm}/>
+            </td>
+            <td >
+              <div>
+                <WikimediaSearch inputRef={inputRef} 
+                  searchHistory={searchHistory} setSearchHistory={setSearchHistory}
+                  searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+              </div>
+            </td>
+          </tr>
+        </tbody>
       </table>
     </div>
-
   );
 }
 
